e7fb7c4f3a35ff767445ba0b2f157b187bd10bda
Vyron Arvanitis <vyron.arvanitis@cern.ch>
2025-08-20 19:13:53 +0300

    first simple example is working

--- bde/models/FNN_Builder.py
+++ bde/models/FNN_Builder.py
@@ -1,9 +1,7 @@
 # FNN_Builder.py
 import jax
 import jax.numpy as jnp
-import optax
 
-from bde.training.FNN_Trainer import FNN_Trainer
 
 class FNN():
     'Builds a single FNN'
@@ -21,34 +19,3 @@ class FNN():
             params.append((W, b))
         self.params = params
         return params
-
-
-
-def main():
-    # generate True data for test purposes
-    main_key = jax.random.PRNGKey(0)
-    k_X, k_W, k_eps = jax.random.split(main_key, 3)
-    X_true = jax.random.normal(k_X, (1024, 10))
-    true_W = jax.random.normal(k_W, (10, 1))
-    y_true = X_true @ true_W + 0.1 * jax.random.normal(k_eps, (1024, 1))
-
-    sizes = [10, 64, 64, 1]
-
-    model = FNN(sizes)
-    trainer = FNN_Trainer()
-    
-    # Create optimizer
-    optimizer = optax.adam(learning_rate=0.01)
-    
-    trainer.fit(
-        model=model,
-        X=X_true,
-        y=y_true,
-        optimizer=optimizer,
-        epochs=1000
-    )
-
-    y_pred = trainer.predict(model.params, X_true)
-    print("the first predictions are ", y_pred)
-if __name__ == "__main__":
-    main()
---
+++ examples/fnn_example.py
@@ -0,0 +1,36 @@
+import jax
+import jax.numpy as jnp
+from bde.models.FNN_Builder import FNN
+from bde.training.FNN_Trainer import FNN_Trainer
+import optax
+
+def main():
+    # generate True data for test purposes
+    main_key = jax.random.PRNGKey(0)
+    k_X, k_W, k_eps = jax.random.split(main_key, 3)
+    X_true = jax.random.normal(k_X, (1024, 10))
+    true_W = jax.random.normal(k_W, (10, 1))
+    y_true = X_true @ true_W + 0.1 * jax.random.normal(k_eps, (1024, 1))
+
+    sizes = [10, 64, 64, 1]
+
+    model = FNN(sizes)
+    trainer = FNN_Trainer()
+
+    # Create optimizer
+    optimizer = optax.adam(learning_rate=0.01)
+
+    trainer.fit(
+        model=model,
+        X=X_true,
+        y=y_true,
+        optimizer=optimizer,
+        epochs=1000
+    )
+
+    y_pred = trainer.predict(model.params, X_true)
+    print("the first predictions are ", y_pred)
+
+
+if __name__ == "__main__":
+    main()
