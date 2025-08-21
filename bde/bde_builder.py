c04e4aca3d34ae06b89c731a10cc89086c3cb1a5
Vyron Arvanitis <vyron.arvanitis@cern.ch>
2025-08-21 12:05:03 +0300

    fit method of bde builder done

--- bde/bde_builder.py
+++ bde/bde_builder.py
@@ -2,11 +2,12 @@
 
 from models.models import Fnn
 from training.trainer import FnnTrainer
+import optax
 
 
 class BdeBuilder(Fnn, FnnTrainer):
     # TODO: build the BdeBuilderClass
-    def __init__(self, sizes, n_members, epochs):
+    def __init__(self, sizes, n_members, epochs, optimizer):
         Fnn.__init__(self, sizes)
         FnnTrainer.__init__(self)
         self.sizes = sizes
@@ -14,6 +15,7 @@ class BdeBuilder(Fnn, FnnTrainer):
         self.epochs = epochs
 
         self.members = []
+        self.optimizer = optimizer or optax.adam(learning_rate=0.01)
 
     def get_model(self, seed: int) -> Fnn:
         """Create a single Fnn model and initialize its parameters
@@ -47,8 +49,19 @@ class BdeBuilder(Fnn, FnnTrainer):
         self.members = [self.get_model(seed) for seed in range(self.n_members)]
         return self.members
 
-    def store_ensemble_results(self):
-        pass
+    def fit(self, model, X, y, optimizer, epochs=100):
+        """Train each member of the ensemble
 
-    def fit(self):
+        Parameters
+        ---------
+        #TODO: documentation
+        """
+        if not self.members:
+            self.deep_ensemble_creator()
+
+        for member in self.members:
+            super().fit(model=member, X=X, y=y, optimizer=self.optimizer, epochs=epochs or self.epochs)
+        return self.members
+
+    def store_ensemble_results(self):
         pass
