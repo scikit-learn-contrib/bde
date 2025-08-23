ed7b81c414ab1e12cba5c0b54e61b1fcac6b3772
Vyron Arvanitis <vyron.arvanitis@cern.ch>
2025-08-23 13:23:09 +0300

    added a todo

--- examples/fnn_example.py
+++ examples/fnn_example.py
@@ -47,7 +47,7 @@ def main():
     bde = BdeBuilder(sizes, n_members=3, epochs=500, optimizer=optax.adam(1e-2))
     print(bde)
     # fit + predict
-    bde.fit(x=data.x, y=data.y, optimizer=bde.optimizer, epochs=500, model=None)
+    bde.fit(x=data.x, y=data.y, optimizer=bde.optimizer, epochs=500, model=None) #TODO: model=None, needs to be fixed!
     bde_pred = bde.predict_ensemble(test_set.x, include_members=True)
     # print(out["ensemble_mean"])
     # print(out["ensemble_var"])
