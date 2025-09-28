061063e8a0b719b7717244422965e24dead490b1
Vyronas Arvanitis <vyronas.arvanitis@gmail.com>
2025-09-28 18:48:29 +0300

    sort of working documentation!

--- bde/__init__.py
+++ bde/__init__.py
@@ -2,7 +2,8 @@
 # License: BSD 3 clause
 
 from ._template import TemplateClassifier, TemplateEstimator, TemplateTransformer
-#from ._version import __version__
+# from ._version import __version__
+from .bde import BdeRegressor, BdeClassifier, Bde, BdePredictor
 
 __version__ = "0.1"
 
@@ -10,5 +11,9 @@ __all__ = [
     "TemplateEstimator",
     "TemplateClassifier",
     "TemplateTransformer",
+    "BdeRegressor",
+    "BdeClassifier",
+    "Bde",
+    "BdePredictor",
     "__version__",
 ]
