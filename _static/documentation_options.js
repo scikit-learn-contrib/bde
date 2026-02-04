767dbf7726de49ece407fa78a51bae6af5ba13bf
vyron-arvanitis <vyron-arvanitis@users.noreply.github.com>
2026-02-04 09:16:43 +0000

    [ci skip] Merge pull request #70 from scikit-learn-contrib/dependabot/github_actions/actions-229d4e1b45
    Bump the actions group with 2 updates bbf01d840602e6b2b7d0163f0bc73a48817b48e5

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.0.2.dev51+gf8bf11df0',
+    VERSION: '1.0.2.dev53+gbbf01d840',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
