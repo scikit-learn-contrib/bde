13168f7501d3b471a31de59b8fa6c88c16600598
EmanuelSommer <EmanuelSommer@users.noreply.github.com>
2026-07-15 05:05:26 +0000

    [ci skip] Merge pull request #89 from scikit-learn-contrib/joss-feedback
    ♻️ improve benchmark reproducibility 3ac5800434b1368fe65cf8d1918398627e8a28a6

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.0.2.dev75+g28e7e24f4',
+    VERSION: '1.0.2.dev77+g3ac580043',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
